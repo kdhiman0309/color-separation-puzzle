5b9a3a1cdeed47c4a613d30827f9195143d3901d
Richa Banker <richa.7b@gmail.com>
2018-03-12 03:39:11 -0700

    pre processing main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import graph
 import brute_force
+import time
 
 g = graph.GraphBuilder()
 g.build_graph(file_path="board_3x3_1.txt")
 #g.build_random_graph(num_rows=3, num_cols=3)
+#g.save_to_file("board_01.txt")
 g.print_board()
 
 print("####################### BEFORE PRE PROCESSING ###############")
+start=time.time()
 btf = brute_force.BruteForceSolver(g.square_graph, g.corner_graph)
 path = btf.solve()
+end=time.time()
+
+time_BF=end-start
 print("\npath:: ",end="")
 for p in path:
     print(p.to_string(), end=" -> ")
@@ -23,9 +29,16 @@ p = graph.PreprocessGraph(g)
 p.preprocess(g)
 
 print("####################### AFTER PRE PROCESSING ###############")
+start=time.time()
 btf = brute_force.BruteForceSolver(g.square_graph, g.corner_graph)
 path = btf.solve()
+end=time.time()
+
+time_PP=end-start
 print("\npath:: ",end="")
 for p in path:
     print(p.to_string(), end=" -> ")
 print()
+
+
+print("TIME BF: ", time_BF, " TIME PP: ",time_PP)
